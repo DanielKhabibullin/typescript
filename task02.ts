8efcced426648fa3d1346a1d0f7d6c1f4d1b05ee
Daniel Khabibullin <i6unny@gmail.com>
2023-06-19 18:29:42 +0500

    Add task02

--- task02.ts
+++ task02.ts
@@ -34,6 +34,17 @@ export abstract class Users<T extends User>{
 		const user = this.userList.find((user) => user['id'] === id);
 		return user ?? null;
 	}
+
+	sorted(prop: keyof T, order: 'asc' | 'desc' = 'asc'): T[] {
+		const sortedList = [...this.userList].sort((a, b) => {
+			if (order === 'asc') {
+				return a[prop] > b[prop] ? 1 : -1;
+			} else {
+				return a[prop] < b[prop] ? 1 : -1;
+			}
+		});
+		return sortedList;
+	}
 }
 
 export class Students extends Users<Student> {
@@ -60,14 +71,19 @@ students.add({ id: 2, firstname: 'Anna', surname: 'Norton', age: 21, year: 3, sp
 students.add({ id: 3, firstname: 'Thomas', surname: 'Anderson', age: 19, year: 1, specialty: 'Biology' });
 students.add({ id: 4, firstname: 'William', surname: 'Smith', age: 22, year: 4, specialty: 'Chemistry' });
 students.add({ id: 5, firstname: 'Tom', surname: 'Cruise', age: 18, year: 1, specialty: 'English' });
-console.log('true', students.remove(3));
-console.log('false', students.remove(3));
-console.log('null', students.get(3));
-console.log('data:', students.get(1));
+// console.log('true', students.remove(3));
+// console.log('false', students.remove(3));
+// console.log('null', students.get(3));
+// console.log('data:', students.get(1));
+const sortedByAgeAsc = students.sorted('age');
+console.log(sortedByAgeAsc);
+const sortedByFirstnameDesc = students.sorted('firstname', 'desc');
+console.log(sortedByFirstnameDesc);
 
 const employees = new Employees();
 employees.add({ id: 1, firstname: 'Mark', surname: 'Twain', age: 30, post: 'Writer' });
 employees.add({ id: 2, firstname: 'Bjarne', surname: 'Stroustrup ', age: 35, post: 'Developer' });
-console.log('false', employees.remove(3));
-console.log('true', employees.remove(1));
-console.log('data:', employees.get(2));
+// console.log('false', employees.remove(3));
+// console.log('true', employees.remove(1));
+// console.log('data:', employees.get(2));
+
